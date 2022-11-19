4b84a39b46f7dfe18d7d790562fd9fb9f9d5aecb
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2022-11-19 14:08:55 +0300

    router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Страница SCP-191 */
+router.get('/SCP-191', function(req, res, next) {
+  res.send("<h1>Страница SCP-191</h1>")
 });
+
+/* Страница SCP-005 */
+router.get('/SCP-005', function(req, res, next) {
+  res.send("<h1>Страница SCP-005</h1>")
+});
+
+/* Страница SCP-808 */
+router.get('/SCP-808', function(req, res, next) {
+  res.send("<h1>Страница SCP-808</h1>")
+});
+
 
 module.exports = router;
