8ed906937f5f8377efb89578f60fcfc151807be9
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2022-11-20 15:40:18 +0300

    CREATING A DATA MODULE

--- createDB.js
+++ createDB.js
@@ -7,11 +7,11 @@ const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("threecats");
+var database = client.db("scp");
 database.dropDatabase()
-database = client.db("threecats");
-const cats = database.collection("cats");
-const result = await cats.insertOne({name:"Барсик"});
+database = client.db("scp");
+const SCP = database.collection("cats");
+const result = await SCP.insertOne({name:"scp"});
 console.log(`${result} documents were inserted`);
 } finally {
 await client.close();
