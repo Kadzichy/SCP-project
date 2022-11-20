774b2e8b2aa7525a5d649d170bb9e2cbd70c4c0c
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2022-11-20 15:49:17 +0300

    CONNECTING THE DATABASE

--- createDB.js
+++ createDB.js
@@ -5,11 +5,11 @@ const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("SCP");
+var database = client.db("scp-191");
 database.dropDatabase()
-database = client.db("SCP");
+database = client.db("scp-191");
 const scp = database.collection("scp");
-const result = await scp.insertOne({name:"scp"});
+const result = await scp.insertOne({name:"scp-191"});
 console.log(`${result} documents were inserted`);
 } finally {
 await client.close();
