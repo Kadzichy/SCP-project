1def43d1fcfaf82d58dd88728bffde6c112862fd
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2022-11-28 12:42:42 +0300

    EXTRACTING DATA FROM THE DATABASE

--- routes/scp.js
+++ routes/scp.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var scp = require("../models/scp").SCP
+//var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +9,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница scp */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get('/:nick', function(req, res, next) {
+    SCP.findOne({nick:req.params.nick}, function(err,scp){
+        if(err) return next(err)
+        if(!scp) return next(new Error("Нет такого объекта"))
+        res.render('scp', {
+            title: scp.title,
+            picture: scp.avatar,
+            desc: scp.desc
+        })
+    })
 });
+
 
 module.exports = router;
