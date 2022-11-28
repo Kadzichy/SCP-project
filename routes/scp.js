9b40449ef9b50cc0666afc29efed7f7361c44da1
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2022-11-28 13:00:06 +0300

    FILLING IN THE MENU FROM THE DATABASE

--- routes/scp.js
+++ routes/scp.js
@@ -1,26 +1,35 @@
-/*
-var express = require('express');
-var router = express.Router();
-var scp = require("../models/scp").SCP
-//var async = require("async")
+var express = require('express')
+var router = express.Router()
+var scp = require("../models/scp").scp
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Новый маршрутизатор, для маршрутов, начинающихся с scp');
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с god')
 });
 
 /* Страница scp */
 router.get('/:nick', function(req, res, next) {
-    SCP.findOne({nick:req.params.nick}, function(err,scp){
-        if(err) return next(err)
-        if(!scp) return next(new Error("Нет такого объекта"))
-        res.render('scp', {
-            title: scp.title,
-            picture: scp.avatar,
-            desc: scp.desc
+    async.parallel([
+            function(callback){
+                scp.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                scp.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var scp = result[0]
+            var scp = result[1] || []
+            if(!scp) return next(new Error("Нет такого объекта"))
+            res.render('scp', {
+                title: scp.title,
+                picture: scp.avatar,
+                desc: scp.desc,
+                menu: scp
+            });
         })
-    })
-});
-/*
+})
 
-module.exports = router;
+module.exports = router
