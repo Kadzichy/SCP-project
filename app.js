7ffe0a47cb34777626c7084ac04d56910da49edc
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-01-08 13:55:16 +0300

    CREATING A SESSION

--- app.js
+++ app.js
@@ -2,19 +2,33 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/Kratos')
-var session = require("express-session")
 var logger = require('morgan');
-var scps = require('./routes/scps');
-var scp = require("./models/scp").scp
+var mysql2 = require('mysql2/promise');
+var session = require('express-session')
+var MySQLStore = require('express-mysql-session')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var scps = require('./routes/scps');
+
+// var MongoStore = require('connect-mongo');(session);
 
 var app = express();
 
+var options = {
+  host : '127.0.0.1',
+  port: '3306',
+  user : 'root',
+  password : '12345',
+  database: 'SCP'
+  };
+  var connection = mysql2.createPool(options)
+  var sessionStore = new MySQLStore( options, connection);
+
+  
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,36 +37,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/gods', gods);
-var MongoStore = require('connect-mongo');(session);
+
+
 app.use(session({
-    secret: "SCP",
-    cookie:{maxAge:60*1000},
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({mongoUrl: 'mongodb://localhost/Kratos'})
-}))
-
+  secret: 'SCP',
+  key: 'sid',
+  store: sessionStore,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { 
+  path: '/',
+  httpOnly: true,
+  maxAge: 60*1000
+  }
+  }));
+  
 app.use(function(req,res,next){
-    req.session.counter = req.session.counter +1 || 1
-    next()
-})
-
-app.use(function(req,res,next){
-    res.locals.nav = []
-    scp.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
+  req.session.counter = req.session.counter +1 || 1
+  next()
 })
 
 app.use(require("./middleware/createMenu.js"))
 app.use(require("./middleware/createUser.js"))
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/scps', scps);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -67,11 +78,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title:'Ошибка'});
+  res.render('error', {title:'404'});
 });
 
-app.engine('ejs',require('ejs-locals'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
 
 module.exports = app;
