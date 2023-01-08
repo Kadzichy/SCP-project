b4f97ea3db7c76296e2ae09dbbcf99b73f9010a3
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-01-08 13:43:38 +0300

    FILLING IN THE MENU FROM THE DATABASE

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,12 +1,11 @@
-var scp = require("./../models/scp").scp
-
+var db = require('../mySQLConnect.js');
 
 module.exports = function(req,res,next){
-    res.locals.nav = []
-
-    scp.find(null,{_id:0,title:1,nick:1},function(err,result){
+res.locals.nav = []
+    db.query(`SELECT * FROM gods`, (err, scps) => {  
         if(err) throw err
-        res.locals.nav = result
+        res.locals.nav = scps
         next()
+        
     })
 }
